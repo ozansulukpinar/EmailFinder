848b1d00b01f9e0c64c2716c21a98658433ad2b6
ozansulukpinar <ozansulukpinar@hotmail.com>
2023-07-12 21:47:51 +0300

    Second commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# EmailFinder
+
+A simple app created by using HTML, CSS and JavaScript
+
+## Table of contents
+
+- [General info](#general-info)
+- [Status](#status)
+- [Contact](#contact)
+
+## General info
+
+As of January 2023, there are over 100 million developers in GitHub. With the helping of this app, you can search and find e-mail address of any of all these.
+
+## Status
+
+Project is finished.
+
+## Contact
+
+Created by [@ozansulukpinar](https://github.com/ozansulukpinar) - feel free to contact me!
--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 function searchUsername() {
+    var emails = [];
     var username = $("#username").val();
+    var url = "https://api.github.com/users/username-to-here/events/public";
+    url = url.replace("username-to-here", username);
 
-    if (username != null || username != "") {
-        var url = "https://api.github.com/users/xxxxxxxx/events/public";
-        url = url.replace("xxxxxxxx", username);
-
-        $.getJSON(url, function (data) {
-            try {
-                if (data != null) {
-
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "jsonp",
+        success: function (result) {
+            if (result.data.length === undefined || result.data.length === 0) {
+                if (result.data.message) {
+                    $("#response").val("There is no username like that.");
                 } else {
-
+                    $("#response").val("There is no data for that user.");
                 }
-            } catch {
-
+            } else {
+                result.data.forEach(function (item) {
+                    if (item.payload.commits) {
+                        var commits = item.payload.commits;
+                        commits.forEach(function (element) {
+                            emails.push("\n" + element.author.email);
+                        });
+                    }
+                });
+                if (emails.length > 0) {
+                    let uniqueEmails = emails.filter((entity, index) => {
+                        return emails.indexOf(entity) === index;
+                    });
+                    $("#response").val(uniqueEmails);
+                } else {
+                    $("#response").val("There is no e-mail info for that user.");
+                }
             }
-        });
-    }
-    else {
-        alert("You need to enter an username to be able to searching.");
-    }
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            $("#response").val("An error happens during call the service.");
+        }
+    });
 }
